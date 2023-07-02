bf1ea46e4135dc58e4997f6990d1d9c861ae8d2b
NE21-sekine <ne211072@sensgu-u.jp>
2023-07-03 00:34:55 +0900

    mybot create

---
+++ mybot/scripts/ping.js
@@ -0,0 +1,15 @@
+'use strict';
+module.exports = (robot) => {
+  robot.respond(/PING$/i, (res) => {
+    res.send('PONG');
+  });
+  robot.respond(/ADAPTER$/i, (res) => {
+    res.send(robot.adapterName);
+  });
+  robot.respond(/ECHO (.*)$/i, (res) => {
+    res.send(res.match[1]);
+  });
+  robot.respond(/TIME$/i, (res) => {
+    res.send(`Server time is: ${new Date()}`);
+  });
+};
